7167f0015aa9098b1f672e062397c405692ff8e4
wasdx19 <vanrwei@gmail.com>
2019-10-12 17:42:41 +0600

    asp.net core mvc routing

---
+++ asp.netProject/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace asp.netProject.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CalculatorController : Controller
+    {
+        [Route("{firstNumber:int}/{secondNumber:int}")]
+        public IActionResult Sum(int firstNumber, int secondNumber)
+        {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '+';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber + secondNumber;
+
+            return View("Result");
+        }
+
+        [Route("{firstNumber:int}/{secondNumber:int:min(1)}")]
+        public IActionResult Divide(int firstNumber, int secondNumber)
+        {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '/';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber / secondNumber;
+
+            return View("Result");
+        }
+    }
+}
---
+++ asp.netProject/Controllers/MessagesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace asp.netProject.Controllers
+{
+    [Route("Say")]
+    public class MessagesController : Controller
+    {
+        [Route("{*message}")]
+        public IActionResult ShowMessage(string message)
+        {
+            if (string.IsNullOrEmpty(message))
+            {
+                ViewData["Message"] = "Message is empty";
+            }
+            else
+            {
+                ViewData["Message"] = message;
+            }
+
+            return View();
+        }
+    }
+}

40d17f1406d2cf6a2406b94811ea0fe77bd8e6f2
wasdx19 <vanrwei@gmail.com>
2019-10-12 18:32:36 +0600

    Asp.net core mvc views

---
+++ asp.netProject/Models/Movies/EmptyClass.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace asp.netProject.Models.Movies
+{
+    public class EmptyClass
+    {
+        public EmptyClass()
+        {
+        }
+    }
+}
---
+++ asp.netProject/Models/Movies/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace asp.netProject.Models.Movies
+{
+    public class Movie
+    {
+        [Display(Name = "Id")]
+        public int Id { get; set; }
+        [Display(Name = "Movie name")]
+        public string Name { get; set; }
+        [Display(Name = "Movie author")]
+        public string Author { get; set; }
+        [Display(Name = "Movie genre")]
+        public string Genre { get; set; }
+        [Display(Name = "URL to poster")]
+        public string Poster { get; set; }
+        [Display(Name = "Release date")]
+        public DateTime CreatedAt { get; set; }
+    }
+}
